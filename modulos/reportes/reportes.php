1d7742ed4cff0ee5c513406d055e1fd85de4bef5
whary11 <whary11@gmail.com>
2017-08-14 21:30:38 -0500

    Se creó el modulo para los reportes

--- modulos/reportes/reportes.php
+++ modulos/reportes/reportes.php
@@ -5,48 +5,42 @@ sleep(2);
 	require_once ("../../conexion.php");
 	$mes = $_POST['mes'];
 
-	printf($mes);
-
-	$q = "SELECT * FROM orden WHERE estado_pago_id='1' AND fecha_entrega LIKE UPPER('%Agosto%')";
-
-	$resultado = $conn->query($q);
-
-	print(json_encode($resultado));
+	
 
 
 
 
-// $totalMes = '$ 1.000.000';
-// 	print('<div class="col s6 m6 animated slideInUp">
-// 			<h2 class="header"></h2>
-// 		<div class="card horizontal hoveraved">
-// 		  <div class="card-image">
-// 		  </div>
-// 		  <div class="card-stacked">
-// 		    <div class="card-content">
-// 		      <p>En el mes de <span id="mes" align="justify"><b>'.$mes.'</b></span> se han recibido hasta el momento <b>'.$totalMes.'</b> de pesos corespondientes a los trabajos realizados.</p>
-// 		    </div>
-// 		    <div class="card-action">
-// 		      <a href="#">Reporte mensual</a>
-// 		    </div>
-// 		  </div>
-// 		</div>
-// 	</div>');
-// 	print('<div class="col s6 m6 animated slideInUp">
-// 			<h2 class="header"></h2>
-// 		<div class="card horizontal hoveraved">
-// 		  <div class="card-image">
-// 		  </div>
-// 		  <div class="card-stacked">
-// 		    <div class="card-content">
-// 		      <p>Se realizaron <b>4</b> pantalones, <b>4</b> blusas y <b>13</b> shores a <b>7</b> clientes.</p>
-// 		    </div>
-// 		    <div class="card-action">
-// 		      <a href="#">Reporte mensual</a>
-// 		    </div>
-// 		  </div>
-// 		</div>
-// 	</div>');
+$totalMes = '$ 1.000.000';
+	print('<div class="col s6 m6 animated slideInUp">
+			<h2 class="header"></h2>
+		<div class="card horizontal hoveraved">
+		  <div class="card-image">
+		  </div>
+		  <div class="card-stacked">
+		    <div class="card-content">
+		      <p>En el mes de <span id="mes" align="justify"><b>'.$mes.'</b></span> se han recibido hasta el momento <b>'.$totalMes.'</b> de pesos corespondientes a los trabajos realizados.</p>
+		    </div>
+		    <div class="card-action">
+		      <a href="#">Reporte mensual</a>
+		    </div>
+		  </div>
+		</div>
+	</div>');
+	print('<div class="col s6 m6 animated slideInUp">
+			<h2 class="header"></h2>
+		<div class="card horizontal hoveraved">
+		  <div class="card-image">
+		  </div>
+		  <div class="card-stacked">
+		    <div class="card-content">
+		      <p>Se realizaron <b>4</b> pantalones, <b>4</b> blusas y <b>13</b> shores a <b>7</b> clientes.</p>
+		    </div>
+		    <div class="card-action">
+		      <a href="#">Reporte mensual</a>
+		    </div>
+		  </div>
+		</div>
+	</div>');
 // }
 
  ?>
