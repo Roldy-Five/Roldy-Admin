14bcb9eca481ef1b5b7b94439aae8d6241dfb614
whary11 <whary11@gmail.com>
2017-08-20 09:42:09 -0500

    Se realizó el fronend del inicio de sesión

---
+++ modulos/inicio/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Roldy Five</title>
+	<link rel="stylesheet" type="text/css" href="../../css/materialize.css">
+	<link rel="stylesheet" type="text/css" href="../../css/iconos.css">
+
+
+	<style type="text/css">
+		body{
+			background-image:url('../../img/fondo.jpg');
+			background-repeat: no-repeat;
+
+		}
+		
+
+
+	</style>
+</head>
+<body>
+	<div class="row" style="margin-top:100px;opacity:.7">
+	    <form class="col s6 card offset-s3">
+		<h4 class="center-align">Inicio de sesión</h4>
+		    <div class="row">
+		        <div class="input-field col s6 offset-s3">
+		          <i class="material-icons prefix">account_circle</i>
+		          <input id="usuario" type="text" class="validate">
+		          <label for="usuario">Usuario</label>
+		        </div>
+		    </div>
+			<div class="row">
+		        <div class="input-field col s6 offset-s3">
+		          <i class="material-icons prefix">phone</i>
+		          <input id="contrasena" type="password" class="validate">
+		          <label for="contrasena">Contraseña</label>
+		        </div>
+	        </div>
+	        <div class="row">
+		        <div class="col s2 offset-s4">
+		          <button class="waves-effect waves-light btn">Iniciar</button>
+		        </div>
+	        </div>
+	    </form>
+	</div>
+	<a href=""><p class="center-align">Terminos y condiciones.</p></a>
+</body>
+<script type="text/javascript" src="../../js/jquery-3.2.1.js"></script>
+<script type="text/javascript" src="../../js/materialize.js"></script>
+</html>
