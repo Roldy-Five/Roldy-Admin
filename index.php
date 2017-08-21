847df721f4cd079a2ddfb8b359cb4f132359eb8b
David Raga Renteria <davidragarenteria@gmail.com>
2017-08-21 16:38:59 -0500

    cerrar sesion y otros

--- index.php
+++ index.php
@@ -1,109 +1,46 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
+	<meta charset="UTF-8">
 	<title>Roldy Five</title>
-	<meta name="viewport" content="width=device-width,user-escalabel=no,initial-scale=1.0, maximum-scale=1.0, minimun-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
-	<link rel="stylesheet" type="text/css" href="css/estilos.css">
-	<link rel="stylesheet" type="text/css" href="css/iconos.css">		
-	<link rel="stylesheet" type="text/css" href="css/toastr.min.css">
-	<link rel="stylesheet" type="text/css" href="css/animate.css">
-	
-	<script src="node_modules/sweetalert/dist/sweetalert.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="node_modules/sweetalert/dist/sweetalert.css">
+	<link rel="stylesheet" type="text/css" href="css/materialize.css">
+	<link rel="stylesheet" type="text/css" href="css/iconos.css">
 </head>
 <body>
-
-	<header>
-		<ul id="dropdown1" class="dropdown-content">
-			<li><a href="#!">Ordenes por realizar</a></li>
-			<li><a href="index.php?modulo=reportes&elemento=index.php">Total por mes</a></li>
-			<!--<li class="divider"></li>-->
-			</ul>
-			<ul id="dropdown2" class="dropdown-content">
-			<li><a href="#!">Ordenes por realizar</a></li>
-			<li><a href="index.php?modulo=reportes&elemento=index.php">Total por mes</a></li>
-			<!--<li class="divider"></li>-->
-			</ul>
-
-			<ul id="dropdown3" class="dropdown-content">
-			<li><a href="index.php?modulo=orden&elemento=index.php">Nueva orden</a></li>
-			<li><a href="index.php?modulo=abonos&elemento=index.php">Realizar pagos</a></li>
-			<!--<li class="divider"></li>-->
-			</ul>
-			<ul id="dropdown4" class="dropdown-content">
-			<li><a href="index.php?modulo=orden&elemento=index.php">Nueva orden</a></li>
-			<li><a href="index.php?modulo=abonos&elemento=index.php">Realizar pagos</a></li>
-			<!--<li class="divider"></li>-->
-			</ul>
-
-		<nav>
-		    <div class="nav-wrapper teal darken-2">
-
-		    
-		      <span><i class="material-icons left large">person</i>David Raga Renteria</span>
-		      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-		      <ul class="right hide-on-med-and-down">
-		        <li><a href="index.php?modulo=cliente&elemento=index.php"><i class="material-icons left large">supervisor_account</i>Cliente</a></li>
-		        <li><a href="index.php?modulo=medidas&elemento=index.php"><i class="material-icons left large">assignment</i>Medidas</a></li> 
-		        <li><a class="dropdown-button" href="#!" data-activates="dropdown3"><i class="material-icons left large">shopping_cart</i>Orden<i class="material-icons right">arrow_drop_down</i></a></li>
-		       	<li><a class="dropdown-button" href="#!" data-activates="dropdown2"><i class="material-icons left">trending_down</i>Reportes<i class="material-icons right">arrow_drop_down</i></a></li>
-		      </ul>
-		      <ul class="side-nav" id="mobile-demo">
-		        <li><a href="index.php?modulo=cliente&elemento=index.php"><i class="material-icons left large">supervisor_account</i>Cliente</a></li>
-		        <li><a href="index.php?modulo=medidas&elemento=index.php"><i class="material-icons left large">assignment</i>Medidas</a></li> 
-		        <li><a class="dropdown-button" href="#!" data-activates="dropdown4"><i class="material-icons left large">shopping_cart</i>Orden<i class="material-icons right">arrow_drop_down</i></a></li>
-		       	<li><a class="dropdown-button" href="#!" data-activates="dropdown1"><i class="material-icons left">trending_down</i>Reportes<i class="material-icons right">arrow_drop_down</i></a></li>
+	<div class="row" style="margin-top:100px;opacity:.7">
+	    <form class="col s8 m4 card offset-s2 offset-m4" id="inicio_sesion">
+		<h4 class="center-align">Inicio de sesión</h4>
+		    <div class="row">
+		        <div class="input-field col s12 m12">
+		          <i class="material-icons prefix">person</i>
+		          <input id="usuario" type="text" class="validate">
+		          <label for="usuario">Usuario</label>
+		        </div>
 		    </div>
- 		 </nav>
-	</header>
-	<main>
-		
-	<div class="container" style="margin-bottom:2%;">
-		<?php
-			$mod = @$_GET['modulo'];
-			$m=@$_GET['elemento'];
-			$archivo = 'modulos/'.$mod.'/'.$m;
-			if (file_exists($archivo) and !empty($_GET['modulo']) and !empty($_GET['elemento'])) {
-				include_once($archivo);
-			}else{
-				include_once("modulos/orden/index.php");
-			}
-		?>
+			<div class="row">
+		        <div class="input-field col s12 m12">
+		          <i class="material-icons prefix"><i class="material-icons">visibility_off</i></i>
+		          <input id="contrasena" type="password" class="validate">
+		          <label for="contrasena">Contraseña</label>
+		        </div>
+	        </div>
+	        <div class="row">
+		        <div class="col s2 m2 offset-m4 offset-s2">
+		          <button class="waves-effect waves-light btn">Iniciar</button>
+		        </div>
+	        </div>
+	    </form>
 	</div>
+	<a href=""><p class="center-align">Terminos y condiciones.</p></a>
 
-		
-	</main>
 
-	<footer class="page-footer teal darken-2">
-      <div class="">
-        <div class="row">
-          <div class="col l6 s12 offset-l3">
-            <h5 class="white-text" align="center">Modisteria Roldy Five</h5>
-            <p class="white-text text-lighten-4" align="center">Desarrollado por David Raga Renteria y Luis Fernando Raga Renteria</p>
-          </div>
-         <!--  <div class="col l4 offset-l2 s12">
-            <h5 class="white-text">Links</h5>
-          </div> -->
-        </div>
-      </div>
-      <div class="footer-copyright">
-        <div class="container">
-        Todos los derechos reservados © 2017 Copyright
-        <a class="grey-text text-lighten-4 right" href="#!">Ayuda</a>
-        </div>
-      </div>
-    </footer>
+	
 </body>
+<script type="text/javascript" src="js/jquery-3.2.1.js"></script>
+<script type="text/javascript" src="js/materialize.js"></script>
+<script type="text/javascript" src="js/accion_inicio.js"></script>
 
 
-	<!-- <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script> -->
-	<script src="js/materialize.min.js"></script>
-	<script type="text/javascript" src="js/toastr.min.js"></script>
-	<script >
-		$(".button-collapse").sideNav();
-		$('.solo-numero').keyup(function (){
-			this.value = (this.value + '').replace(/[^0-9]/g, '');
-		});
-	</script>
+
+
 </html>
--- js/accion_inicio.js
+++ js/accion_inicio.js
@@ -5,11 +5,12 @@ $(document).ready(function() {
 	formulario.submit(function(event) {
 		event.preventDefault();
 		//Validación de datos antes de ser enviados por ajax
+		//alert(usuario);
 		if (usuario.val()==""){
-			 Materialize.toast('Diligenciar capo usuario !!', 3000)
+			 Materialize.toast('Diligenciar capo usuario !!', 3000);
 
 		}else if(contrasena.val()==""){
-			 Materialize.toast('Diligenciar capo contraseña !!', 3000)
+			 Materialize.toast('Diligenciar capo contraseña !!', 3000);
 		}else{
 			//JSON con los datos a enviar por ajax
 			var datos = {
@@ -17,14 +18,18 @@ $(document).ready(function() {
 				contrasena:contrasena.val()
 			}
 			$.ajax({
-				url: 'valida_usuario.php',
+				url: 'modulos/inicio/valida_usuario.php',
 				type: 'POST',
-				dataType:'HTML',
+				//dataType:'HTML',
 				data:datos,
 				beforeSend:""
 			})
 			.done(function(data) {
-				//Código a ejecutar segú la respuesta positiva del servidor
+				if (data == true) {
+					window.location.replace("index2.php");
+				}else if (data == false)  {
+					Materialize.toast('Usuario o contraeña invalidos !!', 3000);
+				}
 			})
 			.fail(function(data) {
 				//Se ejecutará si haya algún error de conexión
